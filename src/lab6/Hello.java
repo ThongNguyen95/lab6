49de2ce5872d3dbc911b480e52191f3f1803cf1c
zarni <zarni@134.154.53.48>
2019-03-01 14:37:34 -0800

    commit_2

--- src/lab6/Hello.java
+++ src/lab6/Hello.java
@@ -12,6 +12,6 @@ package lab6;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Thong Nguyen's version");
-        System.out.println("Zarni's version");
+        System.out.println("Zarni's version 0.1");
     }
 }
