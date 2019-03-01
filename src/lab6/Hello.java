1c1fe2b15964bf0168064933f5ab022ab5175f5b
Thong Nguyen <Thong Nguyen@134.154.48.247>
2019-03-01 14:48:05 -0800

    Merge origin/master
    Conflicts:
    	src/lab6/Hello.java

--- src/lab6/Hello.java
+++ src/lab6/Hello.java
@@ -11,7 +11,7 @@ package lab6;
  */
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Thong Nguyen's version");
+        System.out.println("Thong Nguyen's version 2.0");
         System.out.println("Zarni's version 0.1");
         System.out.println("Eric version");
         System.out.println("Myo version");
